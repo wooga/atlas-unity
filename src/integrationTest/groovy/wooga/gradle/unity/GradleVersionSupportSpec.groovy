a2dde63edaf29740b2bb0c8b5a2ba10ffcc7f0c2
Manfred Endres <manfred.endres@tslarusso.de>
2017-06-26 10:12:13 +0200

    Add gradle version support tests
    The `GradleVersionSupportSpec` will test basic plugin apply and `build`
    tasks against a list of gradle versions.
    
    Document supported versions in README

---
+++ src/integrationTest/groovy/wooga/gradle/unity/GradleVersionSupportSpec.groovy
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package wooga.gradle.unity
+
+import spock.lang.Unroll
+
+class GradleVersionSupportSpec extends UnityIntegrationSpec {
+
+    def gradleVersions() {
+        ["2.14", "3.0", "3.1", "3.2", "3.4", "3.4.1", "3.5", "3.5.1", "4.0"]
+    }
+
+    @Unroll("verify plugin activation with gradle #gradleVersionToTest")
+    def "activates with multiple gradle versions"() {
+        given: "a buildfile with unity plugin applied"
+        buildFile << """
+            group = 'test'
+            ${applyPlugin(wooga.gradle.unity.UnityPlugin)}
+        """.stripIndent()
+
+        gradleVersion = gradleVersionToTest
+
+        expect:
+        runTasksSuccessfully("tasks")
+
+        where:
+        gradleVersionToTest << gradleVersions()
+    }
+
+    @Unroll("verify build task works with gradle #gradleVersionToTest")
+    def "runs basic tasks with multiple gradle versions"() {
+        given: "a buildfile with unity plugin applied"
+        buildFile << """
+            group = 'test'
+            ${applyPlugin(wooga.gradle.unity.UnityPlugin)}
+        """.stripIndent()
+
+        gradleVersion = gradleVersionToTest
+
+        expect:
+        runTasksSuccessfully("build")
+
+        where:
+        gradleVersionToTest << gradleVersions()
+    }
+}
