b20439249491fac07510837db65a9065b6377e63
Christian Sagel <ckpsm@live.com>
2021-06-01 11:41:32 +0200

    Add missing licenses

--- src/main/groovy/wooga/gradle/unity/tasks/Unity.groovy
+++ src/main/groovy/wooga/gradle/unity/tasks/Unity.groovy
@@ -1,7 +1,29 @@
-package wooga.gradle.unity.tasks
+/*
+ * Copyright 2021 Wooga GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package wooga.gradle.unity.tasks
 
 import wooga.gradle.unity.UnityTask
 
+import javax.inject.Inject
+
 class Unity extends UnityTask {
+
+    @Inject
+    Unity() {
+        description = "The default unity task, which can be configured to execute any command line option"
+    }
 }
