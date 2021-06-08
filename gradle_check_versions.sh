dd259e15c10f990475e2067a3a4be93b3fb7a96e
Manfred Endres <manfred.endres@tslarusso.de>
2021-06-08 11:55:49 +0200

    Update gradle to 6.9 and net.wooga.plugins to 2.1.1
    Description
    ===========
    
    Just a simple update to the latest gradle version.
    It also updates to the latest version of
    `net.wooga.plugins`
    
    Changes
    =======
    
    * ![UPDATE] ![GRADLE] to `6.9`
    * ![UPDATE] `net.wooga.plugins` to `2.1.1`

--- gradle_check_versions.sh
+++ gradle_check_versions.sh
@@ -16,7 +16,7 @@
 # limitations under the License.
 #
 
-versions=("")
+versions=("5.1" "5.2" "5.3" "5.4" "5.5" "5.6" "6.0" "6.1" "6.2" "6.3" "6.4" "6.5" "6.6" "6.7" "6.8")
 
 rm -fr build/reports
 for i in "${versions[@]}"
