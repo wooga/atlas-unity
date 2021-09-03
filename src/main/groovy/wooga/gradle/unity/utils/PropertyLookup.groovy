3b5f7efddd8e5f1add6c0f2272fdc24ad02ed0a1
Manfred Endres <manfred.endres@tslarusso.de>
2021-09-03 13:05:11 +0200

    Fix removed PropertyLookup API
    Description
    ===========
    
    The patch [#110] removed the class `wooga.gradle.unity.utils.PropertyLookup`
    in favor of `com.wooga.gradle.PropertyLookup` from the new library
    `com.wooga.gradle:gradle-commons`. This introduced a breaking API change
    as some downstream projects are using the utility class from this
    package. I added the class back but instead of adding a copy I
    extended the new util from `gradle-commons`.
    I marked this class as deprecated to discurage the further usage.
    
    Changes
    =======
    
    * ![FIX] revert `PropertyLookup` API with a standin class

---
+++ src/main/groovy/wooga/gradle/unity/utils/PropertyLookup.groovy
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2018-2020 Wooga GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package wooga.gradle.unity.utils
+
+import groovy.transform.InheritConstructors
+
+/**
+ * Utility object to help fetch properties from environment/gradle properties.
+ * <p>
+ * This implementation is deprecated.
+ * Please use {@code com.wooga.gradle.PropertyLookup} from {@code gradle-commons}
+ * @see https://github.com/wooga/gradle-commons
+ */
+@InheritConstructors
+@Deprecated
+class PropertyLookup extends com.wooga.gradle.PropertyLookup {
+}
